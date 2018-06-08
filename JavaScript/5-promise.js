989e0509d1e7a7b7ac29975f19b463442ad95d85
tshemsedinov <timur.shemsedinov@gmail.com>
2018-06-08 23:31:50 +0300

    Add Promise example

---
+++ JavaScript/5-promise.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const sum = (a, b) => new Promise((resolve, reject) => {
+  if (typeof(a) === 'number' && typeof(b) === 'number') {
+    resolve(a + b);
+  } else {
+    reject(new Error('a and b should be numbers'));
+  }
+});
+
+sum(2, 3)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+
+sum(7, 'A')
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
